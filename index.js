e4413b0b7c53e6e3733254fda129911cfa8676a0
Tomara Petty <tomararuth@gmail.com>
2020-12-13 20:58:18 -0800

    added questions, and md file layout.

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is you project title?',
+    },
+    {
+        type: 'input',
+        name: 'projectLink',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Give a short description/overview of your project:',
+      },
+      {
+        type: 'input',
+        name: 'instalation',
+        message: 'What are the steps and instalation instructions to get the development environment running?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        choices: [ "BSD", new inquirer.Separator(), "MIT", new inquirer.Separator(), "GPL" ],
+        message: 'Choose a license:'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage instructions?',
+      },
+      {
+        type: 'input',
+        name: 'ghimage',
+        message: 'Enter the web address of your GitHub profile photo:',
+      }, 
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
 
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,3 +6,36 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+`# ${answers.projectTitle}
+<a href="${answers.prjectLink}">Click here to see it in action.</a>
+
+## Description 
+*Overview of this project.* 
+${answers.description}
+
+## Table of Contents
+* [Usage](#usage)
+* [Installation](#installation)
+* [License](#license)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+*Steps required to install project and get the development environment established.*
+${answers.installation}
+* ${answers.instBullet}
+* ${answers.instBullet}
+
+## Usage
+*Instructions and examples for use:* 
+* ${answers.usage}
+<img src="${answers.image}">
+
+## Questions?
+* <img src="${answers.ghimage}" alt="GitHub profile image">
+* If you have any questions please reach out to me here: 
+* GitHub: ${answers.github} 
+* Email: ${answers.email}
+`;
